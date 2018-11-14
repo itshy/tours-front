5419f29a319870d85625d34aa18bed5384e37380
itshy <ilya07081999@gmail.com>
2018-11-14 15:33:12 +0300

    path routers (api - axios)

--- src/api/index.js
+++ src/api/index.js
@@ -1,39 +1,36 @@
 import axios from 'axios'
 
-export const getUserAccount = () => {
-  return axios.get('http://localhost:3333/api/v1/test')
-}
 
 export const addNewTourDB = (tours) => {
-  return axios.post('http://localhost:3333/tours', tours)
+  return axios.post('http://localhost:3333/api/v1/tours', tours)
 }
 
 export const getToursDB = () => {
   return (
-    axios.get('http://localhost:3333/getTours')
+    axios.get('http://localhost:3333/api/v1/getTours')
   )
 }
 
 export const addComment = (idTour) => {
-  return axios.post('http://localhost:3333/CommentController', idTour)
+  return axios.post('http://localhost:3333/api/v1/CommentController', idTour)
 }
 
 export const getComments = (tourID) => {
-  return axios.get('http://localhost:3333/getComments', { params: { tourID: tourID } })
+  return axios.get('http://localhost:3333/api/v1/getComments', { params: { tourID: tourID } })
 }
 
 export const makeOrder = (tourID) => {
-  return axios.post('http://localhost:3333/MakeOrderController', tourID)
+  return axios.post('http://localhost:3333/api/v1/MakeOrderController', tourID)
 }
 
 export const sortByPriceUp = (countTours) => {
-  return axios.get('http://localhost:3333/SortByPriceUp', { params: { countTours: countTours } })
+  return axios.get('http://localhost:3333/api/v1/SortByPriceUp', { params: { countTours: countTours } })
 }
 
 export const sortByPriceDown = (countTours) => {
-  return axios.get('http://localhost:3333/SortByPriceDown', { params: { countTours: countTours } })
+  return axios.get('http://localhost:3333/api/v1/SortByPriceDown', { params: { countTours: countTours } })
 }
 
 export const loadMore = (countTours) => {
-  return axios.get('http://localhost:3333/LoadMore', { params: { countTours: countTours } })
+  return axios.get('http://localhost:3333/api/v1/LoadMore', { params: { countTours: countTours } })
 }
